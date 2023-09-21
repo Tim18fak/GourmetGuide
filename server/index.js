8222dd53cfa03fc80c09ff0974410af050a4e39b
Timothy Avell <timothy.avell.olatunde@gmail.com>
2023-09-21 19:45:02 +0000

    settting up to deploy on vercel

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 5000;
 require('dotenv').config();
 
 app.use(cors());
-app.use(cors({ origin: 'http://100.25.150.44:3000' }))
 app.use(express.json());
 app.use(express.urlencoded());
 
